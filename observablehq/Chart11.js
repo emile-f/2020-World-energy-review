428332aa275b7e6a972302fde826a1fe1cf5562b
cozenom <59904220+cozenom@users.noreply.github.com>
2022-05-04 16:53:46 -0700

    Update Chart11.js

--- observablehq/Chart11.js
+++ observablehq/Chart11.js
@@ -1,5 +1,5 @@
 import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/d/3824de9a154ffda0@2089.js?v=3";
+import define from "https://api.observablehq.com/@emile-f/project-world-energy-trends@2210.js?v=3";
 window.addEventListener("load", function () {
 new Runtime().module(define, name => {
   if (name === "viewof normalize1") return new Inspector(document.querySelector("#observablehq-viewof-normalize1-e4a57d1b"));
