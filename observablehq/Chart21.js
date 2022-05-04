c64eb3dae0ef92e3ffa0021f33d6973bea6cc231
cozenom <59904220+cozenom@users.noreply.github.com>
2022-05-04 16:55:51 -0700

    Update Chart21.js

--- observablehq/Chart21.js
+++ observablehq/Chart21.js
@@ -1,11 +1,11 @@
 import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/d/3824de9a154ffda0@2089.js?v=3";
+import define from "https://api.observablehq.com/@emile-f/project-world-energy-trends@2210.js?v=3";
 window.addEventListener("load", function () {
-new Runtime().module(define, name => {
-  if (name === "viewof mapsource") return new Inspector(document.querySelector("#observablehq-viewof-mapsource-e4a57d1b"));
-  if (name === "viewof viewpct") return new Inspector(document.querySelector("#observablehq-viewof-viewpct-e4a57d1b"));
-  if (name === "key1") return new Inspector(document.querySelector("#observablehq-key1-e4a57d1b"));
-  if (name === "consumptionmap") return new Inspector(document.querySelector("#observablehq-consumptionmap-e4a57d1b"));
-  if (name === "viewof mapyear") return new Inspector(document.querySelector("#observablehq-viewof-mapyear-e4a57d1b"));
-});
+  new Runtime().module(define, name => {
+    if (name === "viewof mapsource") return new Inspector(document.querySelector("#observablehq-viewof-mapsource-e4a57d1b"));
+    if (name === "viewof viewpct") return new Inspector(document.querySelector("#observablehq-viewof-viewpct-e4a57d1b"));
+    if (name === "key1") return new Inspector(document.querySelector("#observablehq-key1-e4a57d1b"));
+    if (name === "consumptionmap") return new Inspector(document.querySelector("#observablehq-consumptionmap-e4a57d1b"));
+    if (name === "viewof mapyear") return new Inspector(document.querySelector("#observablehq-viewof-mapyear-e4a57d1b"));
+  });
 });
