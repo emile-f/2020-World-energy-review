0ad016a9135cdf9c5fc32343326521dac22b9b2d
cozenom <59904220+cozenom@users.noreply.github.com>
2022-05-04 13:30:27 -0700

    charts

---
+++ observablehq/Chart11.js
@@ -0,0 +1,10 @@
+import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
+import define from "https://api.observablehq.com/d/3824de9a154ffda0@2089.js?v=3";
+window.addEventListener("load", function () {
+new Runtime().module(define, name => {
+  if (name === "viewof normalize1") return new Inspector(document.querySelector("#observablehq-viewof-normalize1-e4a57d1b"));
+  if (name === "sackey1") return new Inspector(document.querySelector("#observablehq-sackey1-e4a57d1b"));
+  if (name === "sac_world") return new Inspector(document.querySelector("#observablehq-sac_world-e4a57d1b"));
+  if (name === "line1") return new Inspector(document.querySelector("#observablehq-line1-e4a57d1b"));
+});
+});
