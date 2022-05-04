568637232835f38d171ce05e27dfb0f8b7e63280
emile ferrand <emile@storied.co>
2022-05-04 15:07:52 -0700

    Add first chart

--- observablehq/chart1.js
+++ observablehq/chart1.js
@@ -1,14 +1,10 @@
 import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/d/3824de9a154ffda0.js?v=3";
-window.addEventListener("load", function () {
+import define from "https://api.observablehq.com/@emile-f/project-world-energy-trends.js?v=3";
+
+window.addEventListener("load", () => {
     new Runtime().module(define, name => {
-        if (name === "viewof typeTruncation") return new Inspector(document.querySelector("#observablehq-viewof-typeTruncation-631ddba6"));
+        if (name === "chartWorld") return new Inspector(document.querySelector("#observablehq-chartWorld-06f59db3"));
+        if (name === "viewof typeTruncation") return new Inspector(document.querySelector("#observablehq-viewof-typeTruncation-67492539"));
         return ["dataForBarChart", "barChartFilter", "worldData", "chartWorld", "countryData", "chart"].includes(name);
     });
-
-    new Runtime().module(define, name => {
-        if (name === "chartWorld") return new Inspector(document.querySelector("#observablehq-chartWorld-0d79b21c"));
-    });
 });
-
-
