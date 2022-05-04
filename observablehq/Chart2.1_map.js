2dae4e90bcd3e06b89da93fce9be615001ebd637
cozenom <59904220+cozenom@users.noreply.github.com>
2022-05-04 13:30:02 -0700

    Delete Chart2.1_map.js

--- observablehq/Chart2.1_map.js
+++
@@ -1,9 +0,0 @@
-import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/d/3824de9a154ffda0@2089.js?v=3";
-new Runtime().module(define, name => {
-  if (name === "viewof mapsource") return new Inspector(document.querySelector("#observablehq-viewof-mapsource-e4a57d1b"));
-  if (name === "viewof viewpct") return new Inspector(document.querySelector("#observablehq-viewof-viewpct-e4a57d1b"));
-  if (name === "key1") return new Inspector(document.querySelector("#observablehq-key1-e4a57d1b"));
-  if (name === "consumptionmap") return new Inspector(document.querySelector("#observablehq-consumptionmap-e4a57d1b"));
-  if (name === "viewof mapyear") return new Inspector(document.querySelector("#observablehq-viewof-mapyear-e4a57d1b"));
-});
