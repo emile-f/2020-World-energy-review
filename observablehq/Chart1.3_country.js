7bc1676f33ea222f0d8f42148995a71801ea31c2
cozenom <59904220+cozenom@users.noreply.github.com>
2022-05-04 13:29:57 -0700

    Delete Chart1.3_country.js

--- observablehq/Chart1.3_country.js
+++
@@ -1,9 +0,0 @@
-import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/d/3824de9a154ffda0@2089.js?v=3";
-new Runtime().module(define, name => {
-  if (name === "viewof countryselect") return new Inspector(document.querySelector("#observablehq-viewof-countryselect-e4a57d1b"));
-  if (name === "viewof normalize2") return new Inspector(document.querySelector("#observablehq-viewof-normalize2-e4a57d1b"));
-  if (name === "sackey2") return new Inspector(document.querySelector("#observablehq-sackey2-e4a57d1b"));
-  if (name === "sac_country") return new Inspector(document.querySelector("#observablehq-sac_country-e4a57d1b"));
-  if (name === "line2") return new Inspector(document.querySelector("#observablehq-line2-e4a57d1b"));
-});
